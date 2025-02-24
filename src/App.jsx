bf1fa3ecc471252a8b67ed85a035b882b66c3c45
waqas-cs <waqastarakai.cs@gmail.com>
2025-02-24 19:59:08 +0400

    start of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import Search from "./components/Search";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("I am a batman");
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You Will Enjoy
+            Without Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   );
 };
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,5 @@
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return <div className="text-white text-3xl">{searchTerm}</div>;
+};
+
+export default Search;
